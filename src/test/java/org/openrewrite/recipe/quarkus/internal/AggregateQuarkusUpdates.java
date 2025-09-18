ac7d4add2e7baa241e7d15768086e39ce064cd94
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-09-18 17:37:22 +0200

    add special handling and fix typos

--- src/test/java/org/openrewrite/recipe/quarkus/internal/AggregateQuarkusUpdates.java
+++ src/test/java/org/openrewrite/recipe/quarkus/internal/AggregateQuarkusUpdates.java
@@ -6,20 +6,25 @@ import org.jetbrains.annotations.Nullable;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.SortedMap;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
+import static java.util.function.Predicate.not;
+
 public class AggregateQuarkusUpdates {
 
-    private static final Path input = Path.of("quarkus-updates/recipes/src/main/resources/quarkus-updates/core");
+    private static final Path input = Path.of("quarkus-updates/recipes/src/main/resources/quarkus-updates");
     private static final Path output = Path.of("src/main/resources/META-INF/rewrite/quarkus-consolidated.yml");
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         System.out.printf("Starting aggregation of Quarkus update recipes from %s to %s%n", input.toAbsolutePath(), output.toAbsolutePath());
 
         if (!input.toFile().exists()) {
-            throw new IllegalStateException("quarkusio/quarkus-updates.git is not available under 'quarkus-updates', lease clone here temporarily");
+            throw new IllegalStateException("quarkusio/quarkus-updates.git is not available under 'quarkus-updates', please clone here temporarily");
         }
 
         System.out.printf("Reading recipes from %s%n", input.toAbsolutePath());
@@ -35,41 +40,51 @@ public class AggregateQuarkusUpdates {
         for (Map.Entry<Version, Set<String>> entry : sortedByVersion.entrySet()) {
             Version current = entry.getKey();
             Set<String> recipes = entry.getValue();
+
+            if (recipes.isEmpty()) {
+                continue;
+            }
+
             System.out.printf("\t%s has %d recipes%n", current, recipes.size());
             recipeYml.append(createRecipe(current, prior, recipes));
             prior = current;
         }
 
-        Files.writeString(output, recipeYml);
+        try {
+            Files.writeString(output, recipeYml);
+            System.out.printf("Wrote aggregating recipes to %s", output.toAbsolutePath());
+        } catch (IOException e) {
+            System.err.printf("Failed to write to %s due to %s", output.toAbsolutePath(), e.getMessage());
+        }
 
-        System.out.printf("Wrote aggregating recipes to %s", output.toAbsolutePath());
     }
 
     private static SortedMap<Version, Set<String>> recipesDefinedInQuarkusRepo() {
-        try (var children = Files.list(input)) {
-            return children
-              .collect(Collectors.toMap(
-                p -> Version.parse(p.getFileName().toString()),
-                AggregateQuarkusUpdates::extractRecipeVersions,
-                (a, b) -> {
-                    a.addAll(b);
-                    return a;
-                },
-                java.util.TreeMap::new));
+        try (var recipesFiles = Files.find(input, 3, (p, a) -> p.getFileName().toString().endsWith(".yml") || p.getFileName().toString().endsWith(".yaml"))) {
+            return recipesFiles.collect(Collectors.toMap(
+              p -> Version.parse(p.getFileName().toString()),
+              AggregateQuarkusUpdates::extractRecipeVersions,
+              (a, b) -> {
+                  a.addAll(b);
+                  return a;
+              },
+              java.util.TreeMap::new));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    /// Normalize recipe name for a given version
     static String recipeNameFor(Version version) {
-        return "org.openrewrite.quarkus.Quarkus_v%s_%s_%s".formatted(version.major, version.minor, version.patch);
+        if (version.patch != null) {
+            return "org.openrewrite.quarkus.MigrateToQuarkus_v%s_%s_%s".formatted(version.major, version.minor, version.patch);
+        } else {
+            return "org.openrewrite.quarkus.MigrateToQuarkus_v%s_%s".formatted(version.major, version.minor);
+        }
     }
 
+    /// Create a recipe that aggregates all the given recipes for a specific version, optionally including the prior version's recipe as well.
     static String createRecipe(Version version, @Nullable Version priorVersion, Set<String> recipeList) {
-        if (recipeList.isEmpty()) {
-            return "";
-        }
-
         return
           // language=YAML
           """
@@ -94,6 +109,7 @@ public class AggregateQuarkusUpdates {
             return compile.matcher(Files.readString(file))
               .results()
               .map(r -> r.group(1))
+              .filter(not("io.quarkus.updates.minio.minio38.UpdateProperties"::equals)) // Exclude this one as it is a duplicate
               .collect(Collectors.toSet());
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -105,12 +121,22 @@ public class AggregateQuarkusUpdates {
          * The recipe name of a Quarkus update recipe contains the target version.
          */
         public static Version parse(String version) {
+            if ("3alpha.yaml".equals(version)) {
+                // Special case for the initial Quarkus 3 alpha recipe
+                return new Version(3, 0, "alpha1");
+            }
+
             String[] parts = version.split("\\.");
             int major = Integer.parseInt(parts[0]);
             int minor = parts.length > 1 ? Integer.parseInt(parts[1]) : 0;
-            ;
-            String maybeMinorVersion = Objects.equals(parts[2], "yaml") ? null : parts[2];
-            return new Version(major, minor, maybeMinorVersion);
+
+            // handle cases like 2.13.yaml
+            if (parts.length < 3 || Objects.equals(parts[2], "yaml")) {
+                return new Version(major, minor, null);
+            }
+
+            // handle cases like 2.13.0.yaml or 2.13.alpha1.yaml
+            return new Version(major, minor, parts[2]);
         }
 
         @Override
