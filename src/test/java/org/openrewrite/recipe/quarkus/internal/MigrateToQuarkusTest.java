b12883928e240c56a5619e705380feb7726476c0
Tim te Beek <tim@moderne.io>
2025-09-21 13:23:48 +0200

    Trigger recipe validation with a unit test

---
+++ src/test/java/org/openrewrite/recipe/quarkus/internal/MigrateToQuarkusTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.recipe.quarkus.internal;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RewriteTest;
+
+class MigrateToQuarkusTest implements RewriteTest {
+
+    @Test
+    void loadRecipe() {
+        rewriteRun(
+          // Merely load the recipe to trigger validation logic
+          spec -> spec.recipeFromResources("org.openrewrite.quarkus.MigrateToQuarkus_v3_0")
+        );
+    }
+}
