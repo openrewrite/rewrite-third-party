03bb78e83c24e529810bcbb94ad8e30c83559fe1
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2025-09-23 16:29:12 +0200

    Fix ordering and version folding (#42)
    * Add tests and fix problems rooted in stream usage.
    
    * apply formatter
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    * apply review feedback
    
    * Run tests when the quarkus-updates module is present
    
    ---------
    
    Co-authored-by: Tim te Beek <tim@moderne.io>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ src/test/java/org/openrewrite/recipe/quarkus/internal/AggregateQuarkusUpdatesTest.java
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2025 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.recipe.quarkus.internal;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIf;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.openrewrite.recipe.quarkus.internal.AggregateQuarkusUpdates.*;
+
+class AggregateQuarkusUpdatesTest {
+
+    private static final AggregateQuarkusUpdates.Version v1_2 = new AggregateQuarkusUpdates.Version(1, 2, 0);
+    private static final AggregateQuarkusUpdates.Version v1_2_3 = new AggregateQuarkusUpdates.Version(1, 2, 3);
+    private static final AggregateQuarkusUpdates.Version v1_3 = new AggregateQuarkusUpdates.Version(1, 3, 0);
+    private static final AggregateQuarkusUpdates.Version v1_3_2 = new AggregateQuarkusUpdates.Version(1, 3, 2);
+    private static final AggregateQuarkusUpdates.Version v1_5_5 = new AggregateQuarkusUpdates.Version(1, 5, 5);
+
+    static boolean quarkusUpdatesPresent() {
+        return Files.isDirectory(Path.of("quarkus-updates"));
+    }
+
+    @EnabledIf("quarkusRecipesPresent")
+    @Nested
+    class ExtractRecipeNames {
+        private static final Path quarkus39 = Path.of("quarkus-updates/recipes/src/main/resources/quarkus-updates/core/3.9.alpha1.yaml");
+        private static final Path mino38Recipe = Path.of("quarkus-updates/recipes/src/main/resources/quarkus-updates/io.quarkiverse.minio/quarkus-minio/3.8.yaml");
+
+        static boolean quarkusRecipesPresent() {
+            return Files.isReadable(quarkus39) && Files.isReadable(mino38Recipe);
+        }
+
+        @Test
+        void quarkus39() {
+            assertThat(extractRecipeNames(quarkus39))
+              .containsExactlyInAnyOrder(
+                "io.quarkus.updates.core.quarkus39.RemovePanacheAnnotationProcessor",
+                "io.quarkus.updates.core.quarkus39.SyncHibernateJpaModelgenVersionWithBOM",
+                "io.quarkus.updates.core.quarkus39.Relocations",
+                "io.quarkus.updates.core.quarkus39.UpdateConfigRoots",
+                "io.quarkus.updates.core.quarkus39.GraalSDK"
+              );
+        }
+
+        @Test
+        void minoSkipsUpdateProperties() {
+            assertThat(extractRecipeNames(mino38Recipe)).containsExactly("io.quarkus.updates.minio.minio38.UpdateAll");
+        }
+    }
+
+    @EnabledIf("quarkusUpdatesPresent")
+    @Test
+    void readRecipesFromCamlQuarkusModule() throws Exception {
+        Path camelQuarkusDir = Path.of("quarkus-updates/recipes/src/main/resources/quarkus-updates/org.apache.camel.quarkus");
+        assertThat(recipesDefinedInQuarkusRepo(camelQuarkusDir))
+          .containsExactlyInAnyOrderEntriesOf(
+            Map.of(
+              new AggregateQuarkusUpdates.Version(3, 0, 0), List.of("io.quarkus.updates.camel.camel40.CamelQuarkusMigrationRecipe", "org.openrewrite.java.camel.migrate.removedExtensions"),
+              new AggregateQuarkusUpdates.Version(3, 8, 0), List.of("io.quarkus.updates.camel.camel44.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 15, 0), List.of("io.quarkus.updates.camel.camel47.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 17, 0), List.of("io.quarkus.updates.camel.camel49.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 18, 0), List.of("io.quarkus.updates.camel.camel410.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 20, 1), List.of("io.quarkus.updates.camel.camel410.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 22, 0), List.of("io.quarkus.updates.camel.camel411.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 24, 0), List.of("io.quarkus.updates.camel.camel412.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 25, 0), List.of("io.quarkus.updates.camel.camel413.CamelQuarkusMigrationRecipe"),
+              new AggregateQuarkusUpdates.Version(3, 26, 0), List.of("io.quarkus.updates.camel.camel414.CamelQuarkusMigrationRecipe")));
+    }
+
+    @Nested
+    class CreateRecipe {
+        @Test
+        void withPredecessor() {
+            String recipe = AggregateQuarkusUpdates.createRecipe(v1_2_3, v1_2, List.of("org.test.r1", "org.test.r2", "org.test.r3"));
+            assertThat(recipe)
+              //language=YAML
+              .isEqualTo("""
+                ---
+                type: specs.openrewrite.org/v1beta/recipe
+                name: org.openrewrite.quarkus.MigrateToQuarkus_v1_2_3
+                displayName: Quarkus Updates Aggregate 1.2.3
+                description: Quarkus update recipes to upgrade your application to 1.2.3.
+                recipeList:
+                  - org.openrewrite.quarkus.MigrateToQuarkus_v1_2_0
+                  - org.test.r1
+                  - org.test.r2
+                  - org.test.r3
+
+                """);
+        }
+
+        @Test
+        void noPredecessor() {
+            String recipe = AggregateQuarkusUpdates.createRecipe(v1_2_3, null, List.of("org.test.r1", "org.test.r2", "org.test.r3"));
+            assertThat(recipe)
+              //language=YAML
+              .isEqualTo("""
+                ---
+                type: specs.openrewrite.org/v1beta/recipe
+                name: org.openrewrite.quarkus.MigrateToQuarkus_v1_2_3
+                displayName: Quarkus Updates Aggregate 1.2.3
+                description: Quarkus update recipes to upgrade your application to 1.2.3.
+                recipeList:
+                  - org.test.r1
+                  - org.test.r2
+                  - org.test.r3
+
+                """);
+        }
+    }
+
+    @Test
+    void nameGeneration() {
+        assertThat(recipeNameFor(v1_2)).isEqualTo("org.openrewrite.quarkus.MigrateToQuarkus_v1_2_0");
+        assertThat(recipeNameFor(v1_2_3)).isEqualTo("org.openrewrite.quarkus.MigrateToQuarkus_v1_2_3");
+    }
+
+    @Nested
+    class Version {
+        @Test
+        void parserVersion() {
+            assertThat(AggregateQuarkusUpdates.Version.parse("1.2.3.yaml")).isEqualTo(v1_2_3);
+            assertThat(AggregateQuarkusUpdates.Version.parse("1.5.5.yaml")).isEqualTo(v1_5_5);
+        }
+
+        @Test
+        void foldVersions() {
+            assertThat(AggregateQuarkusUpdates.Version.parse("1.2.alpha3.yaml")).isEqualTo(v1_2);
+            assertThat(AggregateQuarkusUpdates.Version.parse("1.2.alpha.yaml")).isEqualTo(v1_2);
+            assertThat(AggregateQuarkusUpdates.Version.parse("1.2.yaml")).isEqualTo(v1_2);
+            assertThat(AggregateQuarkusUpdates.Version.parse("1.2.0.yaml")).isEqualTo(v1_2);
+            assertThat(AggregateQuarkusUpdates.Version.parse("1.2.1.yaml")).isNotEqualTo(v1_2);
+        }
+
+        @Test
+        void sortVersions() {
+            assertThat(List.of(v1_3_2, v1_2, v1_3, v1_2_3).stream().sorted())
+              .containsExactly(v1_2, v1_2_3, v1_3, v1_3_2);
+        }
+    }
+
+}
