ca742ac2e15e035be46f0233b2d413ad107aa10f
Tim te Beek <tim@moderne.io>
2026-04-01 22:47:02 +0200

    Remove unused rewrite-maven testCompileOnly dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,8 +44,6 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-java")
     testImplementation("org.openrewrite:rewrite-test")
 
-    testCompileOnly("org.openrewrite:rewrite-maven")
-
     testImplementation("tech.picnic.error-prone-support:error-prone-contrib:${rewriteVersion}:recipes")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.14.2")
     testImplementation("org.junit-pioneer:junit-pioneer:latest.release")
